6b763121d81d8598a0d85adc8a2f1da1c8f17b4f
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-06-26 12:16:16 +0100

    Implementation of Router in the SPA

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../_services/auth.service";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: "app-nav",
@@ -11,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) {}
 
   ngOnInit() {}
 
@@ -19,10 +19,12 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(
       next => {
         this.alertify.success('Logged in successfully');
+
       },
       error => {
         this.alertify.error(error);
-      }
+      },
+      () => { this.router.navigate(['/members'])}
     );
   }
 
@@ -33,6 +35,7 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
 }
